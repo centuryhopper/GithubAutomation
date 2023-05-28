ad9c0c5a770e27540dbff31a5bf44aba364f9ecb
Leo Zhang <leotheasianlion@gmail.com>
2023-05-28 11:38:28 -0400

    Update fallback_automate_github.py

--- fallback_automate_github.py
+++ fallback_automate_github.py
@@ -1,5 +1,6 @@
 from github import Github
 import platform
+import datetime
 
 if platform.system() == "Linux":
     import linux_tools as tools
@@ -42,6 +43,33 @@ def main():
         os.system(f'git remote add origin git@github.com:leozhang1/{repoName}.git') # change 'leozhang1' to your own github username
     #region files you want to add
         os.system(f"echo # {repoName} >> README.md")
+        
+        license_text =f"""
+MIT License
+Copyright (c) {datetime.datetime.now().year} Leo Zhang
+        
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+        
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+        
+        with open("LICENSE.txt", "w") as file:
+            file.write(license_text)
+        os.system(f"echo #  >> LICENSE.txt")
         os.system(f"echo '.DS_Store' >> .gitignore")
         os.system(f"echo '__pycache__' >> .gitignore")
         os.system(f"echo 'secrets.py' >> .gitignore")
